154872487002e341125d2df3d5a3b3dc94c12cf1
Ashwin901 <ashwinprasanna9@gmail.com>
2021-12-09 09:52:34 +0530

    add nodemon

--- Dockerfile
+++ Dockerfile
@@ -4,10 +4,12 @@ WORKDIR /usr/src/app
 
 COPY package*.json ./
 
+RUN npm install -g nodemon
+
 RUN npm install
 
 COPY . .
 
 EXPOSE 3000
 
-CMD [ "node" ,"app.js"]
+CMD [ "npm" ,"run","dev"]
--- app.js
+++ app.js
@@ -25,17 +25,18 @@ mongoose
     });
 
 app.get("/", (req, res) => {
-    res.send("Hello world!!");
+    res.send("Go to /url");
 });
 
 app.get("/url", async (req, res) => {
     const shortUrls = await ShortUrl.find();
+    console.log("Get Url request");
     res.render("home", { shortUrls: shortUrls });
 });
 
 app.post("/createURL", async (req, res) => {
     await ShortUrl.create({ longUrl: req.body.longUrl });
-
+    console.log("Short Url request");
     res.redirect("/url");
 });
 
